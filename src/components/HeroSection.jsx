4715ef16d0f7a828dab129eaf316cd547edd1a29
Stackbit Code Editor <projects@stackbit.com>
2021-11-24 20:00:02 +0000

    HeroSection.jsx: changed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,6 +5,7 @@ export default function HeroSection({ title, subtitle, buttons }) {
     return (
         <div className="block">
             <h1 data-sb-field-path=".title">{title}</h1>
+            <h3 data-sb-field-path=".subtitle">{subtitle}</h3>
             {buttons.map((button, index) => (
                 <Button key={index} {...button} />
             ))}
