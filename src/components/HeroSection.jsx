ce8ca9e749544c9ce95223801742cdf69e901141
rylandking <ryland@stackbit.com>
2021-11-24 13:08:04 -0700

    remove subtitle

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,6 @@ export default function HeroSection({ title, subtitle, buttons }) {
     return (
         <div className="block">
             <h1 data-sb-field-path=".title">{title}</h1>
-            <h3 data-sb-field-path=".subtitle">{subtitle}</h3>
             {buttons.map((button, index) => (
                 <Button key={index} {...button} />
             ))}
